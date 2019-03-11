e2cc7143a2f1d5f96ba2df0fcb53c400caed5c2e
Prathamesh Bapat <47621188+PrathamBapat@users.noreply.github.com>
2019-03-11 19:30:02 +0530

    Update Answer_1.py
    The first question had some errors and was showing an old date.
    The issue has been resolved.

--- Answer_1.py
+++ Answer_1.py
@@ -1,11 +1,14 @@
 from datetime import datetime as dt
 import pytz as py
-from pytz import timezone
+from pytz import timezone, utc
 
-temp_time = dt.utcfromtimestamp(1541962108935000000 // 1000000000) # The temp_time value comes out to be "1974-11-20 16:16:50"
-time = dt(1974, 11, 20, 16, 16, 50, tzinfo=py.utc).strftime('%Y-%m-%d %H:%M:%S %Z%z') # Now the value is taken as our local time of UTC
+
+ # The temp_time value comes out to be "1974-11-20 16:16:50"
+time = dt.utcfromtimestamp(1541962108935000000 // 1000000000).strftime('%Y-%m-%d %H:%M:%S:%f') # Now the value is taken as our local time of UTC
 print time
-rn = timezone('Europe/Zurich') # Using timezone to get CERN local time difference to that of UTC
-time_cern = dt(1974, 11, 20, 16, 16, 50, tzinfo=py.utc).astimezone(rn).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+value = timezone('CET')
+time = dt.strptime(time, '%Y-%m-%d %H:%M:%S:%f')
+# Using timezone to get CERN local time difference to that of UTC
+time_cern = utc.localize(time, is_dst = None).astimezone(value)
 print time_cern
-
+ 
